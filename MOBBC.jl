bfe7fae434a6b6514dbee049c8bb7c2d04c7ef07
steellwind <gabriel.blanchot@etu.unilim.fr>
2024-08-08 19:58:10 +0200

    Add files via upload

--- MOBBC.jl
+++ MOBBC.jl
@@ -8,6 +8,7 @@ import .MultiObjectiveAlgorithms as MOA
 using DataStructures
 using PyPlot
 
+
 const MOI = MathOptInterface
 
 # Lire les données
@@ -15,11 +16,11 @@ data1 = readdlm("instance/euclidA100.tsp")
 data2 = readdlm("instance/euclidB100.tsp")
 
 # Extraire les coordonnées des 5 premières villes pour chaque objectif
-cities1 = data1[7:11, 2:3]
-cities2 = data2[7:11, 2:3]
+cities1 = data1[7:15, 2:3]
+cities2 = data2[7:15, 2:3]
 
 # Créer une matrice de distance pour chaque objectif
-n = 5
+n = 9
 dist_matrix1 = zeros(Int, n, n)
 dist_matrix2 = zeros(Int, n, n)
 for i in 1:n
@@ -28,6 +29,7 @@ for i in 1:n
         dist_matrix2[i, j] = round(sqrt((cities2[i, 1] - cities2[j, 1])^2 + (cities2[i, 2] - cities2[j, 2])^2))
     end
 end
+
 
 model = Model()
 
@@ -41,7 +43,6 @@ for i in 1:n
     @constraint(model, sum(X[:, i]) == 1)  
     @constraint(model, X[i, i] == 0)       
 end
-
 
 
 # Définir les objectifs
@@ -65,8 +66,23 @@ println("Solve complete. Found $n_solutions solution(s)")
 
 # Extraire et afficher toutes les solutions
 objective_values = []
+path_matrices = []
 for i in 1:n_solutions
     push!(objective_values, MOI.get(model, MOI.ObjectiveValue(i)))
+    path_matrix = zeros(Int, n, n)
+    for j in 1:n
+        for k in 1:n
+            path_matrix[j, k] = MOI.get(model, MOI.VariablePrimal(i), X[j, k])
+        end
+    end
+    push!(path_matrices, path_matrix)
+end
+
+# Imprimer les points du front de Pareto dans le terminal
+for (i, point) in enumerate(objective_values)
+    println("Point $i : Objectif 1 = $(point[1]), Objectif 2 = $(point[2])")
+    println("Matrice du chemin optimal : ")
+    println(path_matrices[i])
 end
 
 
